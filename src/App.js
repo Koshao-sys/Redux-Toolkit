65eae93de608e1c5b6ba4bae9089d6ba68f7aa34
Koshao-sys <92449654+Koshao-sys@users.noreply.github.com>
2023-04-04 12:34:40 +0300

    Practice

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import CartContainer from "./components/CartContainer";
+import NavBar from "./components/NavBar";
+
+function App() {
+  return (
+    <main>
+      <NavBar />
+      <CartContainer />
+    </main>
+  );
+}
+export default App;
---
+++ src/features/cart/cartSlice.js
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit";
+import cartItems from '../../cartItems';
+
+const initialState = {
+  cartItems: cartItems,
+  amount:4,
+  total:0,
+  isLoading:true,
+};
+
+const cartSlice = createSlice({
+  name:'cart',
+  initialState,
+  reducers: {
+    clearCart: (state) => {
+      state.cartItems = [];
+    },
+    removeItem: (state, action) => {
+      console.log(action);
+    }
+  }
+});
+
+export const { clearCart, removeItem } = cartSlice.actions;
+
+export default cartSlice.reducer;
